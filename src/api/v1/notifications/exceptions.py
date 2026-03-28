b540741394ed47b084bcc5e31cfdce70dfaf6b8b
Bayek-bit <tiwnk.7w1nk@yandex.ru>
2026-03-28 20:08:49 +0500

    fix: Атрибут user_id у UserNotFoundError

--- src/api/v1/notifications/exceptions.py
+++ src/api/v1/notifications/exceptions.py
@@ -9,5 +9,6 @@ class UserNotFoundError(NotificationError):
     """Пользователь не найден."""
 
     def __init__(self, user_id: uuid.UUID):
+        self.user_id = user_id
         self.message = f"User {user_id} not found"
         super().__init__(self.message)
