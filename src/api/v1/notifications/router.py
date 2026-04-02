f7468d5249b7ee6925044385b103df36e4b449d0
Bayek-bit <tiwnk.7w1nk@yandex.ru>
2026-04-02 13:23:31 +0500

    feat: ручка для создания уведомления

--- src/api/v1/notifications/router.py
+++ src/api/v1/notifications/router.py
@@ -1,19 +1,27 @@
 import uuid
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.v1.auth.dependencies import verify_service_token
 from src.api.v1.notifications.dependencies import get_notification_service
 from src.api.v1.notifications.repository import NotificationRepository
-from src.api.v1.notifications.schemas import NotificationMarkAsRead
+from src.api.v1.notifications.schemas import NotificationMarkAsRead, NotificationCreate
 from src.api.v1.notifications.service import NotificationService
 from src.database import get_db_session
 
 router = APIRouter(
     prefix="/notifications", dependencies=[Depends(verify_service_token)]
 )
+
+
+@router.post("/create_notification", status_code=status.HTTP_201_CREATED)
+async def create_notification(
+    notification: NotificationCreate,
+    service: NotificationService = Depends(get_notification_service),
+):
+    return await service.create_notification(notification)
 
 
 @router.get("/get_notification/{notification_id}")
