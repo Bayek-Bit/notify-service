0c40e8eb47d6aae6ca5b0828b0c69358a4b64010
Bayek-bit <tiwnk.7w1nk@yandex.ru>
2026-04-02 11:59:32 +0500

    feat: Ручка для получения уведомлений пользователя feat: Ручка для прочтения уведомления

--- src/api/v1/notifications/router.py
+++ src/api/v1/notifications/router.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.api.v1.auth.dependencies import verify_service_token
 from src.api.v1.notifications.dependencies import get_notification_service
 from src.api.v1.notifications.repository import NotificationRepository
+from src.api.v1.notifications.schemas import NotificationMarkAsRead
 from src.api.v1.notifications.service import NotificationService
 from src.database import get_db_session
 
@@ -21,3 +22,21 @@ async def get_notification_by_id(
     service: NotificationService = Depends(get_notification_service),
 ):
     return await service.get_notification_by_id(notification_id)
+
+
+@router.get("/get_user_notifications/{user_id}")
+async def get_user_notifications(
+    user_id: uuid.UUID,
+    service: NotificationService = Depends(get_notification_service),
+):
+    return await service.get_user_notifications(user_id)
+
+
+@router.patch("/mark_notification_as_read/{notification_id}")
+async def mark_notification_as_read(
+    notification_id: uuid.UUID,
+    service: NotificationService = Depends(get_notification_service),
+):
+    # Оставляю схему для будущего расширения - добавления тела запроса с доп. полями (read_at, ...)
+    mark_as_read_data = NotificationMarkAsRead(notification_id=notification_id)
+    return await service.mark_notification_as_read(mark_as_read_data)
