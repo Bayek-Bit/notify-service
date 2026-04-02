3bbc9c57e6c87f8c5d9251c9e3b9ff5211892184
Bayek-bit <tiwnk.7w1nk@yandex.ru>
2026-04-02 15:14:17 +0500

    refactor: Добавлены подсказки типов в ответах ручек

--- src/api/v1/notifications/router.py
+++ src/api/v1/notifications/router.py
@@ -1,4 +1,5 @@
 import uuid
+from typing import List
 
 from fastapi import APIRouter, status
 from fastapi.params import Depends
@@ -7,7 +8,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.api.v1.auth.dependencies import verify_service_token
 from src.api.v1.notifications.dependencies import get_notification_service
 from src.api.v1.notifications.repository import NotificationRepository
-from src.api.v1.notifications.schemas import NotificationMarkAsRead, NotificationCreate
+from src.api.v1.notifications.schemas import (
+    NotificationMarkAsRead,
+    NotificationCreate,
+    NotificationResponse,
+)
 from src.api.v1.notifications.service import NotificationService
 from src.database import get_db_session
 
@@ -20,7 +25,7 @@ router = APIRouter(
 async def create_notification(
     notification: NotificationCreate,
     service: NotificationService = Depends(get_notification_service),
-):
+) -> NotificationResponse:
     return await service.create_notification(notification)
 
 
@@ -28,7 +33,7 @@ async def create_notification(
 async def get_notification_by_id(
     notification_id: uuid.UUID,
     service: NotificationService = Depends(get_notification_service),
-):
+) -> NotificationResponse:
     return await service.get_notification_by_id(notification_id)
 
 
@@ -36,7 +41,7 @@ async def get_notification_by_id(
 async def get_user_notifications(
     user_id: uuid.UUID,
     service: NotificationService = Depends(get_notification_service),
-):
+) -> List[NotificationResponse]:
     return await service.get_user_notifications(user_id)
 
 
@@ -44,7 +49,7 @@ async def get_user_notifications(
 async def mark_notification_as_read(
     notification_id: uuid.UUID,
     service: NotificationService = Depends(get_notification_service),
-):
+) -> NotificationResponse:
     # Оставляю схему для будущего расширения - добавления тела запроса с доп. полями (read_at, ...)
     mark_as_read_data = NotificationMarkAsRead(notification_id=notification_id)
     return await service.mark_notification_as_read(mark_as_read_data)
@@ -56,5 +61,5 @@ async def mark_notification_as_read(
 async def delete_notification(
     notification_id: uuid.UUID,
     service: NotificationService = Depends(get_notification_service),
-):
+) -> None:
     await service.delete_notification(notification_id)
