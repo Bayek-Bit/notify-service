49436c3967d6a79b340fcc1d5b4c1e9bfbb73926
Bayek-bit <tiwnk.7w1nk@yandex.ru>
2026-04-02 15:55:24 +0500

    refactor: убраны лишние импорты

--- src/api/v1/notifications/router.py
+++ src/api/v1/notifications/router.py
@@ -3,18 +3,15 @@ from typing import List
 
 from fastapi import APIRouter, status
 from fastapi.params import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.v1.auth.dependencies import verify_service_token
 from src.api.v1.notifications.dependencies import get_notification_service
-from src.api.v1.notifications.repository import NotificationRepository
 from src.api.v1.notifications.schemas import (
     NotificationMarkAsRead,
     NotificationCreate,
     NotificationResponse,
 )
 from src.api.v1.notifications.service import NotificationService
-from src.database import get_db_session
 
 router = APIRouter(
     prefix="/notifications", dependencies=[Depends(verify_service_token)]
